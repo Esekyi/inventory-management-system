335d4dd1ef9f33f2aadd2b22b9c71c9d5547b2f0
esekyi <sskert10@gmail.com>
2022-08-23 19:00:33 +0800

    made changes to form

--- InventoryManagementSys/Form1.Designer.cs
+++ InventoryManagementSys/Form1.Designer.cs
@@ -35,9 +35,9 @@ namespace InventoryManagementSys
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 32F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(880, 473);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Admin Login";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
