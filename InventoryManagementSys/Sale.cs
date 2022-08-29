98c228278604cdda91dfccd45159a15d4b25206b
esekyi <sskert10@gmail.com>
2022-08-28 19:58:46 -0700

    Created Sale Page

---
+++ InventoryManagementSys/Sale.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace InventoryManagementSys
+{
+    public partial class Sale : UserControl
+    {
+        public Sale()
+        {
+            InitializeComponent();
+        }
+    }
+}
