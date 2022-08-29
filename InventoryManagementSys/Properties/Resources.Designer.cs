067e2378bc39440fb655444d55b11fb490f998d3
esekyi <sskert10@gmail.com>
2022-08-28 23:25:11 -0800

    Added New Pictures for sales page #230

--- InventoryManagementSys/Properties/Resources.Designer.cs
+++ InventoryManagementSys/Properties/Resources.Designer.cs
@@ -153,6 +153,16 @@ namespace InventoryManagementSys.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap icons8_in_stock_58 {
+            get {
+                object obj = ResourceManager.GetObject("icons8-in-stock-58", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap icons8_logout_64 {
             get {
                 object obj = ResourceManager.GetObject("icons8-logout-64", resourceCulture);
