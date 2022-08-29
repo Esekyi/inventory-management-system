47f8d784dcea3bce368af01c7a82b300f8310bb0
esekyi <sskert10@gmail.com>
2022-08-28 22:57:44 -0800

    Added Salespage and logreports page

---
+++ InventoryManagementSys/AttendantControls/SalesPage.Designer.cs
@@ -0,0 +1,407 @@
+﻿
+namespace InventoryManagementSys.AttendantControls
+{
+    partial class SalesPage
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BarcodeTxtBox = new System.Windows.Forms.TextBox();
+            this.labelBarcode = new System.Windows.Forms.Label();
+            this.labelProdName = new System.Windows.Forms.Label();
+            this.labelCatergory = new System.Windows.Forms.Label();
+            this.labelQty = new System.Windows.Forms.Label();
+            this.labelUnitPrice = new System.Windows.Forms.Label();
+            this.ProdName = new System.Windows.Forms.Label();
+            this.ProdCategory = new System.Windows.Forms.Label();
+            this.qtyLabel = new System.Windows.Forms.Label();
+            this.UnitPrice = new System.Windows.Forms.Label();
+            this.labelghC = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.listItems = new System.Windows.Forms.ListView();
+            this.CommitBtn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.QtyBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.AmtPaid = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.CashBalance = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.CusName = new System.Windows.Forms.TextBox();
+            this.label12 = new System.Windows.Forms.Label();
+            this.TotalPrice = new System.Windows.Forms.Label();
+            this.Time = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // BarcodeTxtBox
+            // 
+            this.BarcodeTxtBox.Location = new System.Drawing.Point(42, 92);
+            this.BarcodeTxtBox.Multiline = true;
+            this.BarcodeTxtBox.Name = "BarcodeTxtBox";
+            this.BarcodeTxtBox.Size = new System.Drawing.Size(556, 61);
+            this.BarcodeTxtBox.TabIndex = 0;
+            // 
+            // labelBarcode
+            // 
+            this.labelBarcode.AutoSize = true;
+            this.labelBarcode.Location = new System.Drawing.Point(33, 52);
+            this.labelBarcode.Name = "labelBarcode";
+            this.labelBarcode.Size = new System.Drawing.Size(113, 37);
+            this.labelBarcode.TabIndex = 1;
+            this.labelBarcode.Text = "Barcode";
+            // 
+            // labelProdName
+            // 
+            this.labelProdName.AutoSize = true;
+            this.labelProdName.Location = new System.Drawing.Point(978, 52);
+            this.labelProdName.Name = "labelProdName";
+            this.labelProdName.Size = new System.Drawing.Size(193, 37);
+            this.labelProdName.TabIndex = 1;
+            this.labelProdName.Text = "Product Name:";
+            // 
+            // labelCatergory
+            // 
+            this.labelCatergory.AutoSize = true;
+            this.labelCatergory.Location = new System.Drawing.Point(978, 116);
+            this.labelCatergory.Name = "labelCatergory";
+            this.labelCatergory.Size = new System.Drawing.Size(230, 37);
+            this.labelCatergory.TabIndex = 1;
+            this.labelCatergory.Text = "Product Category:";
+            // 
+            // labelQty
+            // 
+            this.labelQty.AutoSize = true;
+            this.labelQty.Location = new System.Drawing.Point(978, 209);
+            this.labelQty.Name = "labelQty";
+            this.labelQty.Size = new System.Drawing.Size(224, 37);
+            this.labelQty.TabIndex = 1;
+            this.labelQty.Text = "Product Quantity:";
+            // 
+            // labelUnitPrice
+            // 
+            this.labelUnitPrice.AutoSize = true;
+            this.labelUnitPrice.Location = new System.Drawing.Point(33, 274);
+            this.labelUnitPrice.Name = "labelUnitPrice";
+            this.labelUnitPrice.Size = new System.Drawing.Size(144, 37);
+            this.labelUnitPrice.TabIndex = 1;
+            this.labelUnitPrice.Text = "Unit Price: ";
+            // 
+            // ProdName
+            // 
+            this.ProdName.AutoSize = true;
+            this.ProdName.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.ProdName.Location = new System.Drawing.Point(1257, 52);
+            this.ProdName.Name = "ProdName";
+            this.ProdName.Size = new System.Drawing.Size(144, 37);
+            this.ProdName.TabIndex = 1;
+            this.ProdName.Text = "Unit Price: ";
+            // 
+            // ProdCategory
+            // 
+            this.ProdCategory.AutoSize = true;
+            this.ProdCategory.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.ProdCategory.Location = new System.Drawing.Point(1257, 116);
+            this.ProdCategory.Name = "ProdCategory";
+            this.ProdCategory.Size = new System.Drawing.Size(138, 37);
+            this.ProdCategory.TabIndex = 1;
+            this.ProdCategory.Text = "Category: ";
+            // 
+            // qtyLabel
+            // 
+            this.qtyLabel.AutoSize = true;
+            this.qtyLabel.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.qtyLabel.Location = new System.Drawing.Point(1257, 209);
+            this.qtyLabel.Name = "qtyLabel";
+            this.qtyLabel.Size = new System.Drawing.Size(132, 37);
+            this.qtyLabel.TabIndex = 1;
+            this.qtyLabel.Text = "Quantity: ";
+            // 
+            // UnitPrice
+            // 
+            this.UnitPrice.AutoSize = true;
+            this.UnitPrice.Location = new System.Drawing.Point(237, 274);
+            this.UnitPrice.Name = "UnitPrice";
+            this.UnitPrice.Size = new System.Drawing.Size(98, 37);
+            this.UnitPrice.TabIndex = 1;
+            this.UnitPrice.Text = "903.00";
+            // 
+            // labelghC
+            // 
+            this.labelghC.AutoSize = true;
+            this.labelghC.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.labelghC.Location = new System.Drawing.Point(182, 278);
+            this.labelghC.Name = "labelghC";
+            this.labelghC.Size = new System.Drawing.Size(62, 32);
+            this.labelghC.TabIndex = 1;
+            this.labelghC.Text = "GHC";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Navy;
+            this.panel1.Location = new System.Drawing.Point(43, 339);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1665, 5);
+            this.panel1.TabIndex = 2;
+            // 
+            // listItems
+            // 
+            this.listItems.BackColor = System.Drawing.SystemColors.MenuBar;
+            this.listItems.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.listItems.HideSelection = false;
+            this.listItems.Location = new System.Drawing.Point(1056, 365);
+            this.listItems.Name = "listItems";
+            this.listItems.Size = new System.Drawing.Size(648, 599);
+            this.listItems.TabIndex = 3;
+            this.listItems.UseCompatibleStateImageBehavior = false;
+            // 
+            // CommitBtn
+            // 
+            this.CommitBtn.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.CommitBtn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.CommitBtn.FlatAppearance.BorderSize = 0;
+            this.CommitBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.CommitBtn.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.CommitBtn.Image = global::InventoryManagementSys.Properties.Resources.icons8_graph_report_50;
+            this.CommitBtn.Location = new System.Drawing.Point(796, 888);
+            this.CommitBtn.Name = "CommitBtn";
+            this.CommitBtn.Size = new System.Drawing.Size(221, 76);
+            this.CommitBtn.TabIndex = 4;
+            this.CommitBtn.Text = "Commit";
+            this.CommitBtn.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.CommitBtn.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.Gold;
+            this.label1.Location = new System.Drawing.Point(42, 359);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(416, 65);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Sales Transaction...";
+            // 
+            // QtyBox
+            // 
+            this.QtyBox.Location = new System.Drawing.Point(285, 200);
+            this.QtyBox.Name = "QtyBox";
+            this.QtyBox.PlaceholderText = "Quantity";
+            this.QtyBox.Size = new System.Drawing.Size(313, 43);
+            this.QtyBox.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(40, 200);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(237, 37);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Purchase Quantity:";
+            // 
+            // AmtPaid
+            // 
+            this.AmtPaid.Location = new System.Drawing.Point(285, 475);
+            this.AmtPaid.Name = "AmtPaid";
+            this.AmtPaid.PlaceholderText = "Quantity";
+            this.AmtPaid.Size = new System.Drawing.Size(340, 43);
+            this.AmtPaid.TabIndex = 0;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(42, 475);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(183, 37);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Amount Paid: ";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(41, 676);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(222, 37);
+            this.label7.TabIndex = 1;
+            this.label7.Text = "Customer Name: ";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(41, 581);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(192, 37);
+            this.label8.TabIndex = 1;
+            this.label8.Text = "Cash Balance:  ";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(284, 581);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(0, 37);
+            this.label9.TabIndex = 1;
+            // 
+            // CashBalance
+            // 
+            this.CashBalance.AutoSize = true;
+            this.CashBalance.ForeColor = System.Drawing.Color.DarkRed;
+            this.CashBalance.Location = new System.Drawing.Point(339, 581);
+            this.CashBalance.Name = "CashBalance";
+            this.CashBalance.Size = new System.Drawing.Size(98, 37);
+            this.CashBalance.TabIndex = 1;
+            this.CashBalance.Text = "903.00";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label11.ForeColor = System.Drawing.Color.DarkRed;
+            this.label11.Location = new System.Drawing.Point(284, 585);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(62, 32);
+            this.label11.TabIndex = 1;
+            this.label11.Text = "GHC";
+            // 
+            // CusName
+            // 
+            this.CusName.Location = new System.Drawing.Point(283, 673);
+            this.CusName.Multiline = true;
+            this.CusName.Name = "CusName";
+            this.CusName.PlaceholderText = "Name";
+            this.CusName.Size = new System.Drawing.Size(341, 58);
+            this.CusName.TabIndex = 0;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Segoe UI Semibold", 10.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label12.ForeColor = System.Drawing.Color.DarkRed;
+            this.label12.Location = new System.Drawing.Point(43, 838);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(182, 37);
+            this.label12.TabIndex = 1;
+            this.label12.Text = "TOTAL (GHC):";
+            // 
+            // TotalPrice
+            // 
+            this.TotalPrice.AutoSize = true;
+            this.TotalPrice.Font = new System.Drawing.Font("Segoe UI Semibold", 10.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.TotalPrice.ForeColor = System.Drawing.Color.Navy;
+            this.TotalPrice.Location = new System.Drawing.Point(231, 838);
+            this.TotalPrice.Name = "TotalPrice";
+            this.TotalPrice.Size = new System.Drawing.Size(99, 37);
+            this.TotalPrice.TabIndex = 1;
+            this.TotalPrice.Text = "209.00";
+            // 
+            // Time
+            // 
+            this.Time.AutoSize = true;
+            this.Time.BackColor = System.Drawing.Color.Transparent;
+            this.Time.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Time.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.Time.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.Time.Location = new System.Drawing.Point(1492, 10);
+            this.Time.Name = "Time";
+            this.Time.Size = new System.Drawing.Size(67, 32);
+            this.Time.TabIndex = 6;
+            this.Time.Text = "Time";
+            // 
+            // SalesPage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(15F, 37F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.Controls.Add(this.Time);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.CommitBtn);
+            this.Controls.Add(this.listItems);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.qtyLabel);
+            this.Controls.Add(this.ProdCategory);
+            this.Controls.Add(this.ProdName);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.labelghC);
+            this.Controls.Add(this.TotalPrice);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.CashBalance);
+            this.Controls.Add(this.UnitPrice);
+            this.Controls.Add(this.labelUnitPrice);
+            this.Controls.Add(this.labelQty);
+            this.Controls.Add(this.labelCatergory);
+            this.Controls.Add(this.labelProdName);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.CusName);
+            this.Controls.Add(this.AmtPaid);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.QtyBox);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.labelBarcode);
+            this.Controls.Add(this.BarcodeTxtBox);
+            this.Font = new System.Drawing.Font("Segoe UI", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Name = "SalesPage";
+            this.Size = new System.Drawing.Size(1740, 985);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox BarcodeTxtBox;
+        private System.Windows.Forms.Label labelBarcode;
+        private System.Windows.Forms.Label labelProdName;
+        private System.Windows.Forms.Label labelCatergory;
+        private System.Windows.Forms.Label labelQty;
+        private System.Windows.Forms.Label labelUnitPrice;
+        private System.Windows.Forms.Label ProdName;
+        private System.Windows.Forms.Label ProdCategory;
+        private System.Windows.Forms.Label qtyLabel;
+        private System.Windows.Forms.Label UnitPrice;
+        private System.Windows.Forms.Label labelghC;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ListView listItems;
+        private System.Windows.Forms.Button CommitBtn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox QtyBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox AmtPaid;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label CashBalance;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.TextBox CusName;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label TotalPrice;
+        private System.Windows.Forms.Label Time;
+    }
+}
---
+++ InventoryManagementSys/AttendantControls/SalesPage.cs
@@ -0,0 +1,30 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace InventoryManagementSys.AttendantControls
+{
+    public partial class SalesPage : UserControl
+    {
+        public SalesPage()
+        {
+            InitializeComponent();
+        }
+
+        private void BarcodeReader(string barcode)
+        {
+            MySqlCommand command;
+            command = new MySqlCommand(barcode, DBConnections.connection);
+            DBConnections.openConnection();
+            command.CommandText = "SELECT `categoryName`,`product_name`, `product_price`, `stock`, `barcode`, `amount_paid` FROM `product`, `transaction` WHERE `barcode` = @barcode AND `trans_ID` = @id";
+            command.Parameters.AddWithValue("@barcode", barcode + "%");
+
+            DBConnections.closeConnection();
+        }
+    }
+}
